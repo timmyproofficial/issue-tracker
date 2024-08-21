6070244c3a613c60164f45cc1fe287c8791c3433
timmyproofficial <timmyshow96@gmail.com>
2024-08-21 18:41:51 +0100

    Set up Radix UI

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,5 +1,11 @@
+import { Button } from '@radix-ui/themes';
+
 const Issues = () => {
-  return <div>Issues</div>;
+  return (
+    <div>
+      <Button>New Issue</Button>
+    </div>
+  );
 };
 
 export default Issues;
