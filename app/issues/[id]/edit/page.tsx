6333b240392d639258918e2cecf02ca8984641fb
timmyproofficial <timmyshow96@gmail.com>
2024-08-27 13:36:07 +0100

    Fix bug in Edit Issue Page

--- app/issues/[id]/edit/page.tsx
+++ app/issues/[id]/edit/page.tsx
@@ -5,7 +5,7 @@ import IssueFormSkeleton from './loading';
 
 const IssueForm = dynamic(() => import('@/app/issues/_components/IssueForm'), {
   ssr: false,
-  loading: <IssueFormSkeleton />,
+  loading: () => <IssueFormSkeleton />,
 });
 
 interface Props {
