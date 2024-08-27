d1bfaabf1897534517787dbda584dbd8ecd94495
timmyproofficial <timmyshow96@gmail.com>
2024-08-27 11:46:39 +0100

    Refactor: Apply the Single Responsibility Principle

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -4,8 +4,8 @@ import { Button, Callout, TextField } from '@radix-ui/themes';
 import { useForm, Controller } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import axios from 'axios';
-// import SimpleMDE from 'react-simplemde-editor';
-import dynamic from 'next/dynamic';
+import SimpleMDE from 'react-simplemde-editor';
+// import dynamic from 'next/dynamic';
 import 'easymde/dist/easymde.min.css';
 import { useRouter } from 'next/navigation';
 import { createIssueSchema } from '@/app/validationSchema';
@@ -13,9 +13,9 @@ import { z } from 'zod';
 import ErrorMessage from '@/app/components/ErrorMessage';
 import Spinner from '@/app/components/Spinner';
 
-const SimpleMDE = dynamic(() => import('react-simplemde-editor'), {
-  ssr: false,
-});
+// const SimpleMDE = dynamic(() => import('react-simplemde-editor'), {
+//   ssr: false,
+// });
 
 type IssueForm = z.infer<typeof createIssueSchema>;
 
